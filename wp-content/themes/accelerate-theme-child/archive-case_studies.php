bd8d49f35a52f9b22606aa7f6bf0819f53b345ea
Lauren Forsyth <47875465+jl4syth@users.noreply.github.com>
2021-05-25 17:38:14 -0700

    Case Studies Archives Update

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the case studies archive.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,44 +14,13 @@
 
 get_header(); ?>
 
-<div id="primary" class="site-content">
-	<div class="main-content" role="main">
-	<?php while ( have_posts() ) : the_post();
-		$size = "full";
-		$services = get_field('services');
-		$client = get_field('client');
-		$link = get_field('site_link');
-		$image_1 = get_field('image_1');
-		$image_2 = get_field('image_2');
-		$image_3 = get_field('image_3'); ?>
-
-	<article class="case-study">
-		<aside class="case-study-sidebar">
-			<h2><?php the_title(); ?></h2>
-			<h5><?php echo $services; ?></h5>
-			<h6>Client:<?php echo $client; ?></h6>
-
-			<?php the_content(); ?>
-			<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
-		</aside>
-
-		<div class="case-study-images">
-			<?php if($image_1) { 
-				echo wp_get_attachment_image( $image_1, $size );
-			} ?>
-
-			<?php if($image_2) { 
-				echo wp_get_attachment_image( $image_2, $size );
-			} ?>
-
-			<?php if($image_3) { 
-				echo wp_get_attachment_image( $image_3, $size );
-			} ?>
-		</div>
-
-	</article>
-
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+			<h3>Is this working??</h3>
+				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
+
 		</div><!-- .main-content -->
 
 	</div><!-- #primary -->
